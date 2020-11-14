1c113e53125b91ea92e408c0e4ef99f23f010aa3
Zahid <zahidalaaudin@gmail.com>
2020-11-14 11:36:11 +0100

    Device tree for lineage-17.1

---
+++ rr_hotdogb.mk
@@ -0,0 +1,54 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from hotdogb device
+$(call inherit-product, device/oneplus/hotdogb/device.mk)
+
+# Inherit some common RR stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+# Inherit Google Play System Update stuff
+$(call inherit-product-if-exists, vendor/google/psu/google-psu.mk)
+
+# Inherit Gapps
+$(call inherit-product, vendor/gapps/gapps.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := rr_hotdogb
+PRODUCT_DEVICE := hotdogb
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := OnePlus 7T
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_CHARACTERISTICS := nosdcard
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+RR_BUILDTYPE := Official
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_WIFI_EXT := true
+
+# Build info
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=OnePlus7T \
+    PRODUCT_NAME=OnePlus7T \
+    PRIVATE_BUILD_DESC="coral-user 11 RP1A.201005.004 6782484 release-keys"
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
