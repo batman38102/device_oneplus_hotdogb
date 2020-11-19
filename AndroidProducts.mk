0822653dd4041a139db4e0ccdb45b03b74e675a4
Zahid <zahidalaaudin@gmail.com>
2020-11-19 14:23:21 +0100

    pixelexperience

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_hotdogb.mk
+    
+COMMON_LUNCH_CHOICES := \
+    aosp_hotdogb-user \
+    aosp_hotdogb-userdebug \
+    aosp_hotdogb-eng \
